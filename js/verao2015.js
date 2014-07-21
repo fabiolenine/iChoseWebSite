bf74e635f3a0e20851a5abfb34c633eeae01bffe
fabiolenine <fabiolenine@icloud.com>
2014-07-21 02:38:42 -0300

    AngularJS
    Implementação o ng-app.

---
+++ js/verao2015.js
@@ -0,0 +1,19 @@
+var verao2015App = angular.module('verao2015App',[]);
+
+verao2015App.controller('envioemailCtrl', function($scope,$http){
+    
+    var $ = jQuery;
+    
+    $scope.enviar = function(){
+        $http.post('/emailverao2015',$scope.envio).success(function(){
+            init();
+        });
+    };
+    
+    var init = function(){
+        $scope.envio = {email:''};
+    };
+    
+    init();
+    
+}
