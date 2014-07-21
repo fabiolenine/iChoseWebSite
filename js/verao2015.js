fe19334d5bcb849800fa8e160eb6ca1be5ee1e9c
fabiolenine <fabiolenine@icloud.com>
2014-07-21 03:16:17 -0300

    AngularJS
    Teste final com sucesso.

--- js/verao2015.js
+++ js/verao2015.js
@@ -3,8 +3,8 @@ function envioemailCtrl($scope,$http,$window){
     var $ = jQuery;
     
     $scope.enviar = function(){
-        $http.post('/emailverao2015',$scope.envio).success(function(data){
-            $windows.console.log(data);
+        $http.post('/emailverao2015',$scope.envio).success(function(){
+            init();
         });
     }
     
