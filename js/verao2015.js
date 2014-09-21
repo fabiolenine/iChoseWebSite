5ec80fb99314697a09d9f84dbfc3f348f9aa9f3e
fabiolenine <fabiolenine@icloud.com>
2014-09-21 17:49:36 -0300

    Verao2015
    Interpretação de true e false.

--- js/verao2015.js
+++ js/verao2015.js
@@ -3,9 +3,11 @@ function envioemailCtrl($scope,$http,$window){
     var $ = jQuery;
     
     $scope.enviar = function(){
+        show();
         $http.post('/emailverao2015',$scope.envio).success(function(retorno)
         {
-            if(retorno=200) {   
+            console.log(retorno);
+            if(retorno){   
                 $scope.show = {sucesso  : true,
                                 alerta  : true};
             }
@@ -19,8 +21,12 @@ function envioemailCtrl($scope,$http,$window){
     
     var init = function(){
         $scope.envio = {email:''};
+        show();
+    };
+    
+    var show = function(){
         $scope.show  = {sucesso : false,
-                        alerta  : true};
+                        alerta  : true};    
     };
     
     init();
