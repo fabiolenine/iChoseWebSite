106899724da33707804dd27dc118085c907bacc0
fabiolenine <fabiolenine@icloud.com>
2014-09-21 16:20:13 -0300

    Verao2015
    Mensagem de sucesso.

--- js/verao2015.js
+++ js/verao2015.js
@@ -5,12 +5,13 @@ function envioemailCtrl($scope,$http,$window){
     $scope.enviar = function(){
         $http.post('/emailverao2015',$scope.envio).success(function()
         {
-            
+            sucesso = true;
         });
     }
     
     var init = function(){
         $scope.envio = {email:''};
+        $scopr.sucesso = false;
     };
     
     init();
