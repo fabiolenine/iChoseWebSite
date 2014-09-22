3d6e38ac63d160fc69c43b2ef0fc8afd232f0a71
fabiolenine <fabiolenine@icloud.com>
2014-09-21 22:23:52 -0300

    Verao2015
    links

--- js/verao2015.js
+++ js/verao2015.js
@@ -1,23 +1,19 @@
 function envioemailCtrl($scope,$http,$window){
     
-    var $ = jQuery;
-    
     $scope.enviar = function(){
         show();
         $http.post('/emailverao2015',$scope.envio).success(function(retorno)
         {
-            console.log(retorno);
             if(retorno){   
-                $scope.show = {sucesso  : true,
-                                alerta  : true};
-            }
-            else
-            {
-                $scope.show = {sucesso  : false,
-                               alerta   : false};
-            }
+                        $scope.show = {sucesso  : true,
+                                       alerta   : true};
+                        }
+            else {
+                        $scope.show = {sucesso  : false,
+                                       alerta   : false};
+                 }
         });
-    }
+                             };
     
     var init = function(){
         $scope.envio = {email:''};
@@ -31,4 +27,4 @@ function envioemailCtrl($scope,$http,$window){
     
     init();
     
-}
+};
