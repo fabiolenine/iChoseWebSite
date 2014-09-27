3e5f5f914f9a2ff1c80904b22cfd539d96897df2
fabiolenine <fabiolenine@icloud.com>
2014-09-27 16:27:49 -0300

    Verao2015
    Geolocalização

--- js/verao2015.js
+++ js/verao2015.js
@@ -2,13 +2,6 @@ function envioemailCtrl($scope,$http,$window){
     
     $scope.enviar = function(){
         show();
-        
-        // Verifica se o browser do usuario tem suporte a Geolocation
-        if ( navigator.geolocation ){
-            navigator.geolocation.getCurrentPosition( function( posicao ){
-                console.log( posicao.coords.latitude, posicao.coords.longitude );
-            });
-        }
 
         $http.post('/emailverao2015',$scope.envio).success(function(retorno)
         {
@@ -21,10 +14,23 @@ function envioemailCtrl($scope,$http,$window){
                                        alerta   : false};
                  }
         });
-                             };
+    };
     
     var init = function(){
-        $scope.envio = {email:''};
+        
+        // Verifica se o browser do usuario tem suporte a Geolocation
+        if ( navigator.geolocation ){
+            navigator.geolocation.getCurrentPosition( function( posicao ){
+                //$scope.envio.location.lat   := posicao.coords.latitude;
+                //$scope.envio.location.lng   := posicao.coords.longitude;
+                //console.log( $scope.envio );
+                console.log(posicao.coords.latitude);
+                console.log(posicao.coords.longitude);
+            });
+        }
+        
+        $scope.envio = {email:'',
+                       location: {lat: posicao.coords.latitude, lng: posicao.coords.longitude}};
         show();
     };
     
